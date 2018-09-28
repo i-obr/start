a171d84f675cfda34db210fb21e0c390e8e95dba
Ivan Obraztsov <i.obraztsov@8bitgroup.com>
2018-09-28 19:38:25 +0300

    suggest

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,9 @@
-import _ from 'lodash';
-import module1 from './module1';
-import module2 from './module2';
-
-const propertyActions = [
-  {
-    name: 'body',
-    check: arg => typeof arg === 'string',
-  },
-  {
-    name: 'children',
-    check: arg => arg instanceof Array,
-  },
-  {
-    name: 'attributes',
-    check: arg => arg instanceof Object,
-  },
-];
-
-const getPropertyAction = arg =>
-  _.find(propertyActions, ({ check }) => check(arg));
-
-console.log(getPropertyAction('children'));
-module1();
-module2();
+$("#fullname").suggestions({
+    token: "443f948858261171188c6ac5173417561c35e1ef",
+    type: "NAME",
+    count: 5,
+    /* Вызывается, когда пользователь выбирает одну из подсказок */
+    onSelect: function(suggestion) {
+        console.log(suggestion);
+    }
+});
